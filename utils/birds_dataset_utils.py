7a9fe1a6fba40c150adbb2dcdd3b335e5c72f1e4
elligalata <lougalata@gmail.com>
2020-03-09 09:09:11 +0100

    Segmentation scratch

--- utils/birds_dataset_utils.py
+++ utils/birds_dataset_utils.py
@@ -11,10 +11,10 @@ import tensorflow_addons as tfa
 DATASET_PATH = "CUB_200_2011/"
 IMAGES_PATH = os.path.join(DATASET_PATH, "images")
 
-IMG_HEIGHT = 128
-IMG_WIDTH = 128
-N_CHANNELS = 3
-N_CLASSES = 200
+IMG_HEIGHT = tf.constant(128, dtype=tf.int32)
+IMG_WIDTH = tf.constant(128, dtype=tf.int32)
+N_CHANNELS = tf.constant(3, dtype=tf.int32)
+N_CLASSES = tf.constant(200, dtype=tf.int32)
 
 # mean = tf.constant([0.431776, 0.499619, 0.485914], dtype=tf.float32)
 mean = tf.constant([0.43174747, 0.49959317, 0.48588511], dtype=tf.float32)
@@ -56,7 +56,7 @@ def get_img(img_path):
     # return tf.image.resize(img, [IMG_HEIGHT, IMG_WIDTH])
 
 
-def get_birds_tf_dataset(data, augmentation=False, aspect_ratio=False):
+def get_birds_tf_dataset(dataset, augmentation=False, aspect_ratio=False):
     """
     This method builds an input pipeline for the birds dataset images applying optional data augmentation
     :param data: Dataframe with the class labels and image paths
@@ -64,16 +64,21 @@ def get_birds_tf_dataset(data, augmentation=False, aspect_ratio=False):
     :param horizontal_flip:
     :return: Unbatched dataset of loaded images and their correspondent one_hot_enconded labels
     """
-    imagepaths = tf.convert_to_tensor(data['img_path'].values, dtype=tf.string)
-    labels = tf.convert_to_tensor(data['class_label'].values, dtype=tf.int32)
+    # imagepaths = tf.convert_to_tensor(data['img_path'].values, dtype=tf.string)
+    # labels = tf.convert_to_tensor(data['class_label'].values, dtype=tf.int32)
+    #
+    # dataset = tf.data.Dataset.from_tensor_slices((imagepaths, labels))
+    # # Load images and set one hot encoded labels
+    # dataset = dataset.map(lambda img_path, label: (get_img(img_path),
+    #                                                tf.one_hot(label - 1, N_CLASSES)))
 
-    dataset = tf.data.Dataset.from_tensor_slices((imagepaths, labels))
-    # Load images and set one hot encoded labels
-    dataset = dataset.map(lambda img_path, label: (get_img(img_path),
-                                                   tf.one_hot(label - 1, N_CLASSES)))
+
 
     if augmentation:
         dataset = augment_dataset(dataset, aspect_ratio)
+        dataset = dataset.map(lambda img, label: (get_segmented_image_train(img), label))
+    else:
+        dataset = dataset.map(lambda img, mask, label: (get_segmented_image_test(img, mask), label))
 
     # Normalization and resizing ______________
     dataset = dataset.map(lambda img, label: (tf.image.resize(img, [IMG_HEIGHT, IMG_WIDTH]), label))
@@ -83,49 +88,96 @@ def get_birds_tf_dataset(data, augmentation=False, aspect_ratio=False):
 
 
 @tf.function
-def rotate_tf(image):
-    if image.shape.__len__() == 4:
-        random_angles = tf.random.uniform(shape=(tf.shape(image)[0],), minval=-np.pi / 4, maxval=np.pi / 4)
-
+def rotate_tf(img):
     # Outputs random values from a uniform distribution, where minval = pi/4 and maxval = pi/4
-    if image.shape.__len__() == 3:
-        random_angles = tf.random.uniform(shape=(), minval=-np.pi / 6, maxval=np.pi / 6)
 
-    return tfa.image.rotate(image, random_angles)
+    random_angles = tf.random.uniform(
+        shape=(),
+        minval=-tf.constant(np.pi) / tf.constant(6, dtype=tf.float32),
+        maxval=tf.constant(np.pi) / tf.constant(6, dtype=tf.float32))
+
+    img_3d = img[:, :, :, 0]
+    mask = img[:, :, :, 1]
+    img_3d = tfa.image.rotate(img_3d, random_angles)
+    mask = tfa.image.rotate(mask, random_angles)
+    return tf.stack([img_3d, mask], axis=3)
 
 
 def crop_resize(img):
-    BATCH_SIZE = 1
-    NUM_BOXES = 1
-    CROP_SIZE = (128, 128)
+    img_3d = img[:, :, :, 0]
+    mask = img[:, :, :, 1]
 
-    img = tf.expand_dims(img, 0)
+    BATCH_SIZE = tf.constant(1, dtype=tf.int32)
+    NUM_BOXES = tf.constant(1, dtype=tf.int32)
+    CROP_SIZE = (tf.constant(128, dtype=tf.int32), tf.constant(128, dtype=tf.int32))
+
+    img_3d = tf.expand_dims(img_3d, 0)
+    mask = tf.expand_dims(mask, 0)
     left_corner = tf.random.uniform(shape=(NUM_BOXES, 2), minval=0.05, maxval=0.2)
     right_corner = tf.random.uniform(shape=(NUM_BOXES, 2), minval=0.8, maxval=0.95)
 
     boxes = tf.concat((left_corner, right_corner), axis=1)
     box_indices = tf.random.uniform(shape=(NUM_BOXES,), minval=0, maxval=BATCH_SIZE, dtype=tf.int32)
-    output = tf.image.crop_and_resize(img, boxes, box_indices, CROP_SIZE)
-    return output[0]
+    img_3d = tf.image.crop_and_resize(img_3d, boxes, box_indices, CROP_SIZE)[0]
+    mask = tf.image.crop_and_resize(mask, boxes, box_indices, CROP_SIZE)[0]
+    return tf.stack([img_3d, mask], axis=3)
+
+def get_aspect_ratio(img):
+    target_height, target_width = 500, 500
+    img_3d = img[:, :, :, 0]
+    mask = img[:, :, :, 1]
+    img_3d = tf.image.resize_with_pad(img_3d, target_height, target_width,
+                                   method=tf.image.ResizeMethod.GAUSSIAN)
+    mask = tf.image.resize_with_pad(mask, target_height, target_width,
+                                   method=tf.image.ResizeMethod.GAUSSIAN)
+    img = tf.stack([img_3d, mask], axis=3)
+    return tf.clip_by_value(img, 0.0, 1.0)
+
+
+def get_horizontal_flip(img):
+    img_3d = img[:, :, :, 0]
+    mask = img[:, :, :, 1]
+    img_3d = tf.image.flip_left_right(img_3d)
+    mask = tf.image.flip_left_right(mask)
+    return tf.stack([img_3d, mask], axis=3)
+
+
+def get_saturated_img(img):
+    img_3d = img[:, :, :, 0]
+    mask = img[:, :, :, 1]
+    img_3d = tf.image.random_saturation(img_3d, lower=1.5, upper=2.0, seed=103)
+    return tf.stack([img_3d, mask], axis=3)
+
+def get_brightness_img(img):
+    img_3d = img[:, :, :, 0]
+    mask = img[:, :, :, 1]
+    img_3d = tf.clip_by_value(tf.image.random_brightness(img_3d, 0.3, seed=None), 0.0, 1.0)
+    return tf.stack([img_3d, mask], axis=3)
+
+def stuck_img_with_mask(img, mask):
+    mask = tf.image.grayscale_to_rgb(mask)
+    img = tf.stack([img, mask], axis=3)
+    return img
+
 
 def augment_dataset(dataset, aspect_ratio = False):
     # Possible augmentations to perform __________________
 
+    dataset = dataset.map(lambda img, mask, label: (stuck_img_with_mask(img, mask), label))
+
     # Keep the aspect ratio filling the gaps with padding
     if aspect_ratio:
-        target_height, target_width = 200, 200
-        dataset = dataset.map(lambda img, label: (tf.clip_by_value(tf.image.resize_with_pad(
-                                                      img,  target_height, target_width, method=tf.image.ResizeMethod.GAUSSIAN, antialias=True)
-                                                      , 0.0, 1.0), label))
+        dataset = dataset.map(lambda img, label: (get_aspect_ratio(img),label))
+
     # Horizontal flip
-    dataset = dataset.concatenate(dataset.map(lambda img, label: (tf.image.flip_left_right(img), label)))
+    dataset = dataset.concatenate(dataset.map(lambda img, label: (get_horizontal_flip(img), label)))
 
     # Change of saturation
     dataset = dataset.concatenate(dataset.map(lambda img, label:
-                                                  (tf.image.random_saturation(img, lower=1.5, upper=2.0, seed=103),label)))
+                                                  (get_saturated_img(img),label)))
     # Brightness
     dataset = dataset.concatenate(dataset.map(lambda img, label:
-                                              (tf.clip_by_value(tf.image.random_brightness(img, 0.3, seed=None),0.0, 1.0), label)))
+                                              (get_brightness_img(img), label)))
 
     # Rotation flip
     rotated_dataset = dataset.map(lambda img, label: (rotate_tf(img), label))
@@ -169,15 +221,52 @@ def load_dataset():
     return train_df, val_df, test_df, classes_df
 
 
-def get_segmentation_dataset(tf_records_dir="CALTECH_BIRDS_2011"):
-    dataset = tfds.load(name="caltech_birds2011",
-                        data_dir=tf_records_dir,
-                        split=None,
-                        shuffle_files=False)
+def get_segmentation_dataset(tf_records_dir="CALTECH_BIRDS_2011", with_info=False):
+    dataset, info = tfds.load(name="caltech_birds2011",
+                              data_dir=tf_records_dir,
+                              split=None,
+                              shuffle_files=False, with_info=True)
     training_dataset = dataset['train']
-    test_dataset = dataset['train']
+    test_dataset = dataset['test']
 
-    training_dataset = training_dataset.map(lambda x: (x['image'], tf.cast(x['segmentation_mask'], tf.bool)))
-    testing_dataset = test_dataset.map(lambda x: (x['image'], tf.cast(x['segmentation_mask'], tf.bool)))
+    # Extract (image, mask, label) tuples
+    training_dataset = training_dataset.map(lambda x:
+                                            (tf.image.convert_image_dtype(x['image'], tf.float32),
+                                             tf.cast(tf.cast(x['segmentation_mask'], tf.bool), tf.float32),
+                                             tf.cast(x['label'], tf.int32)))
+    test_dataset = test_dataset.map(lambda x:
+                                    (tf.image.convert_image_dtype(x['image'], tf.float32),
+                                     tf.cast(tf.cast(x['segmentation_mask'], tf.bool), tf.int8),
+                                     tf.cast(x['label'], tf.int32)))
 
-    return training_dataset, testing_dataset
+
+    # # # Normalize all images ________
+    # training_dataset = training_dataset.map(lambda img, mask, label: (normalize_img(img), mask, label))
+    # test_dataset = test_dataset.map(lambda img, mask, label: (normalize_img(img), mask, label))
+    #
+    # # # Resize to equal size ________
+    # training_dataset = training_dataset.map(lambda img, mask, label:
+    #                                         (tf.image.resize(img, [IMG_HEIGHT, IMG_WIDTH]),
+    #                                          tf.image.resize(mask, [IMG_HEIGHT, IMG_WIDTH]),
+    #                                          label))
+    # test_dataset = test_dataset.map(lambda img, mask, label:
+    #                                 (tf.image.resize(img, [IMG_HEIGHT, IMG_WIDTH]),
+    #                                  tf.image.resize(mask, [IMG_HEIGHT, IMG_WIDTH]),
+    #                                  label))
+    if with_info:
+        return training_dataset, test_dataset, info
+    else:
+        return training_dataset, test_dataset
+
+
+def get_segmented_image_train(img):
+    img_3d = img[:, :, :, 0]
+    mask = img[:, :, :, 1]
+    mask = tf.cast(mask, dtype=tf.float32)
+    result = tf.math.multiply(img_3d, mask)
+    return result
+
+def get_segmented_image_test(img, mask):
+    mask = tf.cast(mask, dtype=tf.float32)
+    result = tf.math.multiply(img, mask)
+    return result
--- visualize_dataset_input_pipeline.py
+++ visualize_dataset_input_pipeline.py
@@ -1,4 +1,4 @@
-from __future__ import absolute_import, division, print_function, unicode_literals
+# from __future__ import absolute_import, division, print_function, unicode_literals
 # %tensorflow_version 2.x
 import tensorflow as tf
 import tensorflow_datasets as tfds
@@ -25,6 +25,9 @@ if __name__ == "__main__":
     current_dir = os.getcwd()
     print("Current directory is ", current_dir)
 
+    train_df, test_df = dataset_utils.get_segmentation_dataset()
+
+    #
     # train_df, val_df, test_df, classes_df = dataset_utils.load_dataset()
     # pd.DataFrame.hist(train_df, bins=200, column="class_label")
     # plt.title("Training set class distribution")
@@ -39,24 +42,36 @@ if __name__ == "__main__":
     # # print(test_df.sample(5))
     # print("Existing classes ")
     # print(classes_df.head(5))
-
-    # birds_tf_dataset = dataset_utils.get_birds_tf_dataset(train_df.head(6),
-    #                                                       rand_saturation=True,
-    #                                                       horizontal_flip=True)
     #
-    # batch_size = 32  # 32 # 64
-    # # Show original image resized samples
-    # birds_tf_dataset = birds_tf_dataset.batch(batch_size)
-    # image_batch, label_batch = next(iter(birds_tf_dataset))
-    # dataset_utils.show_batch(image_batch.numpy(), label_batch.numpy())
+    birds_tf_dataset = dataset_utils.get_birds_tf_dataset(train_df, augmentation=True, aspect_ratio=True)
+
     #
-    # Construct a tf.data.Dataset
-    dataset, dataset_info = tfds.load(name="caltech_birds2011", download=False,
-                                      data_dir="tf_CUB_200_2011", split="false", with_info=True)
+    batch_size = 64 # 32 # 64
+    # Show original image resized samples
+    birds_tf_dataset = birds_tf_dataset.batch(batch_size)
+    image_batch, label_batch = next(iter(birds_tf_dataset))
+    dataset_utils.show_batch(image_batch.numpy(), label_batch.numpy())
 
-    # Build your input pipeline
-    dataset = dataset.shuffle(1024).batch(32).prefetch(tf.data.experimental.AUTOTUNE)
-    for features in dataset.take(1):
-        image, label, mask = features["image"], features["label"], features['segmentation_mask']
 
-        break
+    model = models.Sequential()
+    model.add(layers.Conv2D(32, (3, 3), activation='relu',
+                            input_shape=(dataset_utils.IMG_HEIGHT, dataset_utils.IMG_WIDTH, dataset_utils.N_CHANNELS)))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.MaxPooling2D((2, 2)))
+    model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    model.add(layers.Dense(dataset_utils.N_CLASSES))
+
+    model.summary()
+
+    model.compile(optimizer='adam',
+                  loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+                  metrics=['accuracy'])
+
+    birds_tf_test_dataset = dataset_utils.get_birds_tf_dataset(test_df.take(200))
+
+    history = model.fit(birds_tf_dataset, epochs=10,
+                        validation_data=birds_tf_test_dataset)
+
