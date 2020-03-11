e6e864121c50a93ad29348b777118d02cea67db0
Daniel Ordonez <daniels.ordonez@gmail.com>
2020-03-11 12:28:58 +0100

    tf.Dataset V2 fully functional
    See `example_for_Daniel.py` for use of it

--- example_for_Daniel.py
+++ example_for_Daniel.py
@@ -2,9 +2,11 @@ import tensorflow as tf
 import utils.birds_dataset_utils as dataset_utils
 
 if __name__ == "__main__":
-    train_dataset, test_dataset = dataset_utils.get_segmentation_dataset()
-    train_dataset = dataset_utils.get_birds_tf_dataset(train_dataset.take(6), augmentation=True, with_mask=True)
-    test_dataset = dataset_utils.get_birds_tf_dataset(test_dataset.take(6), with_mask=True)
+
+    train_dataset, val_dataset, test_dataset, classes_df = dataset_utils.load_dataset()
+
+    train_dataset = dataset_utils.pre_process_dataset(train_dataset.take(6), augmentation=True, with_mask=False)
+    test_dataset = dataset_utils.pre_process_dataset(test_dataset.take(6), with_mask=False)
 
     batch_size = 64 # 32 # 64
     # Show original image resized samples
@@ -14,4 +16,4 @@ if __name__ == "__main__":
 
     birds_tf_dataset = test_dataset.batch(batch_size)
     image_batch, mask_batch, label_batch = next(iter(birds_tf_dataset))
-    dataset_utils.show_batch(image_batch.numpy(), mask_batch.numpy(), label_batch.numpy(), show_mask=True)
+    dataset_utils.show_batch(image_batch.numpy(), mask_batch.numpy(), label_batch.numpy(), show_mask=False)
--- utils/birds_dataset_utils.py
+++ utils/birds_dataset_utils.py
@@ -6,10 +6,11 @@ import pandas as pd
 import numpy as np
 import utils.augmentation_functions as augmentation_utils
 import cv2
+import glob
 
-
-DATASET_PATH = "CUB_200_2011/"
+DATASET_PATH = "CUB_200_2011"
 IMAGES_PATH = os.path.join(DATASET_PATH, "images")
+SEGMENTATION_PATH = os.path.join(DATASET_PATH, "segmentations")
 
 IMG_HEIGHT = tf.constant(128, dtype=tf.int32)
 IMG_WIDTH = tf.constant(128, dtype=tf.int32)
@@ -58,48 +59,42 @@ def show_batch(image_batch, mask_batch, label_batch, show_mask=False):
         plt.show()
 
 
-# def get_img(img_path):
-#     img = tf.io.read_file(img_path)
-#     # convert the compressed string to a 3D uint8 tensor
-#     img = tf.image.decode_jpeg(img, channels=3)
-#     # Use `convert_image_dtype` to convert to floats in the [0,1] range.
-#     img = tf.image.convert_image_dtype(img, tf.float32)
-#     # resize the image to the desired size.
-#     return img
-    # return tf.image.resize(img, [IMG_HEIGHT, IMG_WIDTH])
-
-
-def get_birds_tf_dataset(dataset, augmentation=False, with_mask=False):
+def pre_process_dataset(dataset, augmentation=False, with_mask=False):
     if augmentation:
         dataset = augmentation_utils.augment_dataset(dataset)
 
     if with_mask:
         dataset = dataset.map(get_segmented_image)
 
-
     # Normalization and resizing ______________
-    dataset = dataset.map(lambda img, mask, label: (tf.image.resize(img, [IMG_HEIGHT, IMG_WIDTH]), tf.image.resize(mask, [IMG_HEIGHT, IMG_WIDTH]),label))
+    dataset = dataset.map(lambda img, mask, label: (tf.image.resize(img, [IMG_HEIGHT, IMG_WIDTH]),
+                                                    tf.image.resize(mask, [IMG_HEIGHT, IMG_WIDTH]),
+                                                    label))
     dataset = dataset.map(lambda img, mask, label: (normalize_img(img), mask, label))
 
-    #One Hot encoding in labels
+    # One Hot encoding in labels
     dataset = dataset.map(lambda img, mask, label: (img, mask, tf.one_hot(label, N_CLASSES)))
 
     return dataset
 
+
 def load_dataset():
     # Load image labels, training/test label and file path.
-    train_labels = pd.read_csv(DATASET_PATH + "image_class_labels.txt", header=None, sep=" ",
+    train_labels = pd.read_csv(os.path.join(DATASET_PATH, "image_class_labels.txt"), header=None, sep=" ",
                                index_col=0, names=["class_label"])
-    train_test = pd.read_csv(DATASET_PATH + "train_test_split.txt", header=None, sep=" ",
+    train_test = pd.read_csv(os.path.join(DATASET_PATH, "train_test_split.txt"), header=None, sep=" ",
                              index_col=0, names=["is_train"])
-    images_paths = pd.read_csv(DATASET_PATH + "images.txt", header=None, sep=" ", index_col=0,
+    images_paths = pd.read_csv(os.path.join(DATASET_PATH, "images.txt"), header=None, sep=" ", index_col=0,
                                names=["img_path"])
-    # Complete relative path
-    images_paths = images_paths.apply(lambda x: IMAGES_PATH + "/" + x)
+    # Generate path files for segmentation data
+    segmentation_paths = images_paths.apply(lambda x: SEGMENTATION_PATH + os.path.sep + x.str.replace('.jpg', '.png'))
+    segmentation_paths.rename(columns={"img_path": "seg_img_path"}, inplace=True, copy=False)
+    # Generate path files for RGB data
+    images_paths = images_paths.apply(lambda x: IMAGES_PATH + os.path.sep + x)
 
     # Combine dataset_df into single Pandas DataFrame
     # image ID is the index of the dataset_df DataFrame
-    dataset_df = pd.concat((train_labels, train_test, images_paths), axis=1)
+    dataset_df = pd.concat((train_labels, train_test, images_paths, segmentation_paths), axis=1)
 
     train_df = dataset_df[dataset_df["is_train"] == 1]
     test_df = dataset_df[dataset_df["is_train"] == 0]
@@ -107,18 +102,62 @@ def load_dataset():
     # Generate validation set with samples of all classes
     val_df = pd.DataFrame([], columns=train_df.columns)
     for class_id in range(1, 201):
-        class_samples = train_df[train_df["class_label"] == class_id].sample(n=4)
+        class_samples = train_df[train_df["class_label"] == class_id].sample(n=3)
         val_df = pd.concat((val_df, class_samples), axis=0)
 
     train_df = train_df.drop(val_df.index)
 
-    classes_df = pd.read_csv(DATASET_PATH + "classes.txt", header=None, sep=" ", index_col=0,
+    classes_df = pd.read_csv(os.path.join(DATASET_PATH, "classes.txt"), header=None, sep=" ", index_col=0,
                              names=["class"])
 
-    return train_df, val_df, test_df, classes_df
+    # Generate Dataset V2 input pipeline
+    train_dataset = tf_dataset_from_dataframe(train_df)
+    val_dataset = tf_dataset_from_dataframe(val_df)
+    test_dataset = tf_dataset_from_dataframe(test_df)
+
+    return train_dataset, val_dataset, test_dataset, classes_df
 
 
-def get_segmentation_dataset(tf_records_dir="CALTECH_BIRDS_2011", with_info=False):
+def tf_dataset_from_dataframe(data_df):
+    img_paths = tf.convert_to_tensor(data_df['img_path'].values, dtype=tf.string)
+    seg_img_paths = tf.convert_to_tensor(data_df['seg_img_path'].values, dtype=tf.string)
+    labels = tf.convert_to_tensor(data_df['class_label'].values, dtype=tf.int32)
+
+    dataset = tf.data.Dataset.from_tensor_slices((img_paths, seg_img_paths, labels))
+    dataset = dataset.map(lambda img_path, mask_path, label: (get_img(img_path, channels=3, dtype=tf.float32),
+                                                              get_mask(mask_path, channels=1, dtype=tf.float32),
+                                                              label))
+    return dataset
+
+
+def get_img(img_path, channels=3, dtype=tf.float32):
+    img = tf.io.read_file(img_path)
+    # convert the compressed string to a 3D uint8 tensor
+    img = tf.image.decode_jpeg(img, channels=channels)
+    # Use `convert_image_dtype` to convert to floats in the [0,1] range.
+    img = tf.image.convert_image_dtype(img, dtype)
+    return img
+
+
+def get_mask(img_path, channels=1, dtype=tf.float32):
+    img = tf.io.read_file(img_path)
+    # convert the compressed string to a 3D uint8 tensor
+    img = tf.image.decode_png(img, channels=channels)
+    # Binarize mask
+    img = tf.cast(tf.cast(img, tf.bool), tf.float32)
+    # Use `convert_image_dtype` to convert to floats in the [0,1] range.
+    img = tf.image.convert_image_dtype(img, dtype)
+    return img
+
+
+def get_segmented_image(img, mask, label):
+    # mask = tf.cast(mask, dtype=tf.float32)
+    img = tf.math.multiply(img, mask)
+    return img, mask, label
+
+
+@DeprecationWarning
+def load_tf_records(tf_records_dir="CALTECH_BIRDS_2011", with_info=False):
     dataset, info = tfds.load(name="caltech_birds2011",
                               data_dir=tf_records_dir,
                               split=None,
@@ -141,8 +180,3 @@ def get_segmentation_dataset(tf_records_dir="CALTECH_BIRDS_2011", with_info=Fals
     else:
         return training_dataset, test_dataset
 
-
-def get_segmented_image(img, mask, label):
-    # mask = tf.cast(mask, dtype=tf.float32)
-    img = tf.math.multiply(img, mask)
-    return img, mask, label
